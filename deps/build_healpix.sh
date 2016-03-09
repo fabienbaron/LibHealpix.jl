75328879a457a540fc2899fbc3f69ad23224458f
Michael Eastwood <mweastwood@astro.caltech.edu>
2016-03-09 10:11:07 -0800

    tell pkg-config where to look
    also iron out the configure.ac a little bit

--- deps/build_healpix.sh
+++ deps/build_healpix.sh
@@ -3,14 +3,15 @@
 PWD=`pwd`
 PREFIX=$PWD/usr
 HEALPIXDIR=$PWD/downloads/Healpix_3.30
+MY_PKG_CONFIG_PATH=$PKG_CONFIG_PATH:$PREFIX/lib/pkgconfig
 
 cd $HEALPIXDIR/src/C/autotools
 autoreconf --install
-./configure --prefix=$PREFIX
+PKG_CONFIG_PATH=$MY_PKG_CONFIG_PATH ./configure --prefix=$PREFIX
 make install
 
 cd $HEALPIXDIR/src/cxx/autotools
 autoreconf --install
-./configure --prefix=$PREFIX
+PKG_CONFIG_PATH=$MY_PKG_CONFIG_PATH ./configure --prefix=$PREFIX
 make install
 
--- deps/build_wrapper.sh
+++ deps/build_wrapper.sh
@@ -3,9 +3,10 @@
 PWD=`pwd`
 PREFIX=$PWD/usr
 WRAPPERDIR=$PWD/src
+MY_PKG_CONFIG_PATH=$PKG_CONFIG_PATH:$PREFIX/lib/pkgconfig
 
 cd $WRAPPERDIR
 autoreconf --install
-./configure --prefix=$PREFIX
+PKG_CONFIG_PATH=$MY_PKG_CONFIG_PATH ./configure --prefix=$PREFIX
 make install
 
