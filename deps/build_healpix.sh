5d93c5c502019c254f78f44290d9a2e9bd53e014
Michael Eastwood <michael.w.eastwood@gmail.com>
2016-03-09 12:35:56 -0800

    set rpath properly while building healpix
    I'm not sure why this isn't set automatically by libtool, but it isn't.

--- deps/build_healpix.sh
+++ deps/build_healpix.sh
@@ -4,14 +4,15 @@ PWD=`pwd`
 PREFIX=$PWD/usr
 HEALPIXDIR=$PWD/downloads/Healpix_3.30
 MY_PKG_CONFIG_PATH=$PKG_CONFIG_PATH:$PREFIX/lib/pkgconfig
+MY_LDFLAGS=-Wl,-rpath,$PREFIX/lib
 
 cd $HEALPIXDIR/src/C/autotools
 autoreconf --install
-PKG_CONFIG_PATH=$MY_PKG_CONFIG_PATH ./configure --prefix=$PREFIX
+PKG_CONFIG_PATH=$MY_PKG_CONFIG_PATH LDFLAGS=$MY_LDFLAGS ./configure --prefix=$PREFIX
 make install
 
 cd $HEALPIXDIR/src/cxx/autotools
 autoreconf --install
-PKG_CONFIG_PATH=$MY_PKG_CONFIG_PATH ./configure --prefix=$PREFIX
+PKG_CONFIG_PATH=$MY_PKG_CONFIG_PATH LDFLAGS=$MY_LDFLAGS ./configure --prefix=$PREFIX
 make install
 
